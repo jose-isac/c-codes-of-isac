1b48574e4c9ca692a78f2a594c7fe41f63909742
jose-isac <joseisac@tutamail.com>
2025-06-06 22:40:54 -0300

    productinfo.c: output for the entered variables

--- productinfo.c
+++ productinfo.c
@@ -12,5 +12,9 @@ int main(void)
     scanf("%f", &price);
     printf("Enter purchase date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &day, &month, &year);
+
+    printf("\nItem\t\tUnit\t\tPurchase\n");
+    printf("\t\tPrice\t\tDate\n");
+    printf("%-5d\t\t$ %6.2f\t%d/%d/%-5d\n", item, price, day, month, year);
     return EXIT_SUCCESS;
 }
